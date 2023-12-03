efb2db08d310d2e2ac5f0a084c455fd881c3457c
wyman3210 <wyman3210@gmail.com>
2023-12-03 00:17:10 -0500

    filled out the Roommate.java a bit more and added a shoppingList.java class

--- app/src/main/java/edu/uga/cs/roomateshopping/Roommate.java
+++ app/src/main/java/edu/uga/cs/roomateshopping/Roommate.java
@@ -1,8 +1,35 @@
 package edu.uga.cs.roomateshopping;
 
+import java.util.ArrayList;
+
 /**
  * Represents a Roommate who has a cart of items
  */
 public class Roommate {
+
+    private String email;
+    private String name;
+    private ArrayList<Item> purchasing;
+
+    /**
+     * default constructor
+     */
+    public Roommate(){
+        email = "";
+        name = "";
+        purchasing = null;
+    }
+
+    /**
+     * constructor.
+     * @param email the user's email.
+     * @param name the user's given name.
+     */
+    public Roommate(String email, String name)
+    {
+        this.email = email;
+        this.name = name;
+        purchasing = new ArrayList<Item>();
+    }
 
 }
---
+++ app/src/main/java/edu/uga/cs/roomateshopping/ShoppingList.java
@@ -0,0 +1,7 @@
+package edu.uga.cs.roomateshopping;
+
+/**
+ * Represents the current shopping list. Needs Firebase.
+ */
+public class ShoppingList {
+}
