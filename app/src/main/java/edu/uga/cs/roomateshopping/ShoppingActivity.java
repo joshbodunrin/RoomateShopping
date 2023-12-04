21d9d48c7095e3e0a5005f737f6020b6bc961c24
joshbodunrin <83253910+joshbodunrin@users.noreply.github.com>
2023-12-03 22:37:13 -0500

    set up nav drawer, need to do implementaion of fragments now

--- app/src/main/java/edu/uga/cs/roomateshopping/ShoppingActivity.java
+++ app/src/main/java/edu/uga/cs/roomateshopping/ShoppingActivity.java
@@ -1,13 +1,27 @@
 package edu.uga.cs.roomateshopping;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ShoppingActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     private Button viewList;
     private Button myList;
@@ -16,11 +30,28 @@ public class ShoppingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
-        viewList = findViewById(R.id.roomShoppingList);
-        myList = findViewById(R.id.myShoppingList);
-        logOut = findViewById(R.id.logOut);
+        //viewList = findViewById(R.id.roomShoppingList);
+        //myList = findViewById(R.id.myShoppingList);
+        //logOut = findViewById(R.id.logOut);
 
-        logOut.setOnClickListener(new LogOutButtonClickListener());
+        //logOut.setOnClickListener(new LogOutButtonClickListener());
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+
+        navigationView = findViewById( R.id.nvView );
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    selectDrawerItem( menuItem );
+                    return true;
+                });
+
 
     }
 
@@ -32,4 +63,46 @@ public class ShoppingActivity extends AppCompatActivity {
             view.getContext().startActivity(intent);
         }
     }
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,  R.string.drawer_close );
+    }
+
+    public void selectDrawerItem( MenuItem menuItem ) {
+        Fragment fragment = null;
+
+        // Create a new fragment based on the used selection in the nav drawer
+        /*
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.menu_add) {
+            fragment = new TakeQuiz();
+        } else if (itemId == R.id.menu_review) {
+            fragment = new ViewQuizzes();
+        } else if (itemId == R.id.menu_help) {
+            fragment = new HomeScreen();
+        } else if (itemId == R.id.menu_close) {
+            finish();
+        } else {
+            return;
+        }
+
+         */
+
+        // Set up the fragment by replacing any existing fragment in the main activity
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace( R.id.fragmentContainerView, fragment).addToBackStack("main screen" ).commit();
+
+        /*
+        // this is included here as a possible future modification
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked( true );
+        // Set action bar title
+        setTitle( menuItem.getTitle());
+         */
+
+        // Close the navigation drawer
+        drawerLayout.closeDrawers();
+    }
+
 }
